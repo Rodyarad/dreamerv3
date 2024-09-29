eafdb5a34eb74a4fef0e453b894962ca563f8af2
Rodyarad <79752705+Rodyarad@users.noreply.github.com>
2024-09-29 23:44:00 +0300

    Standart DreamerV3 for DMC with circles
    Created colored circles for the background, script for launching Dreamer training on a modified environment

---
+++ training.py
@@ -0,0 +1,83 @@
+import warnings
+from functools import partial as bind
+
+import dreamerv3
+import embodied
+
+warnings.filterwarnings('ignore', '.*truncated to dtype int32.*')
+
+
+def main():
+
+  config = embodied.Config(dreamerv3.Agent.configs['defaults'])
+  config = config.update({
+      **dreamerv3.Agent.configs['size100m'],
+      'logdir': f'~/logdir/{embodied.timestamp()}-example',
+      'run.train_ratio': 32,
+  })
+  config = embodied.Flags(config).parse()
+
+  print('Logdir:', config.logdir)
+  logdir = embodied.Path(config.logdir)
+  logdir.mkdir()
+  config.save(logdir / 'config.yaml')
+
+  def make_agent(config):
+    env = make_env(config)
+    agent = dreamerv3.Agent(env.obs_space, env.act_space, config)
+    env.close()
+    return agent
+
+  def make_logger(config):
+    logdir = embodied.Path(config.logdir)
+    return embodied.Logger(embodied.Counter(), [
+        embodied.logger.TerminalOutput(config.filter),
+        embodied.logger.JSONLOutput(logdir, 'metrics.jsonl'),
+        embodied.logger.TensorBoardOutput(logdir),
+        # embodied.logger.WandbOutput(logdir.name, config=config),
+    ])
+
+  def make_replay(config):
+    return embodied.replay.Replay(
+        length=config.batch_length,
+        capacity=config.replay.size,
+        directory=embodied.Path(config.logdir) / 'replay',
+        online=config.replay.online)
+
+  def make_env(config, env_id=0):
+        from dmc2gym.wrappers import DMCWrapper
+        from embodied.envs import from_gym
+        domain_name = "walker" 
+        task_name = "walk" 
+        resource_files = "idealgas0.mp4" 
+        img_source = "video"
+
+        env = DMCWrapper(
+            domain_name=domain_name,
+            task_name=task_name,
+            resource_files=resource_files,
+            img_source=img_source,     
+        )
+
+        env = from_gym.FromGym(env)
+        env = dreamerv3.wrap_env(env, config)
+        return env
+
+  args = embodied.Config(
+      **config.run,
+      logdir=config.logdir,
+      batch_size=config.batch_size,
+      batch_length=config.batch_length,
+      batch_length_eval=config.batch_length_eval,
+      replay_context=config.replay_context,
+  )
+
+  embodied.run.train(
+      bind(make_agent, config),
+      bind(make_replay, config),
+      bind(make_env, config),
+      bind(make_logger, config), args)
+
+
+if __name__ == '__main__':
+  main()
